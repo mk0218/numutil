897510b81a0edfc2ff323651f5062b9aa2378928
Minkyoung Kim <minkyoung.kim@alsemy.com>
2024-07-16 19:49:23 +0900

    Fixed add_abs and sub_abs implementation. - Added From<&str> trait for Integer.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::{
-    cmp::{self, max},
+    cmp::{self, max, min},
     fmt,
     ops
 };
@@ -12,7 +12,7 @@ enum Sign {
 
 impl Sign {
     fn of(v: i32) -> Self {
-        if v > 0 {
+        if v >= 0 {
             Sign::PLUS
         } else {
             Sign::MINUS
@@ -57,13 +57,19 @@ struct Integer {
     value: Vec<u32>,
 }
 
+/// The maximum u32 value among power of 10s.\
+/// u32::MAX is 4,294,967,295, so it the max value is 1,000,000,000.
+const MAX_POW_10_VALUE: u32 = 1_000_000_000;
+/// Exponent for MAX_POW_10_VALUE.
+const MAX_POW_10_EXP: u32 = 9;
+
 impl Integer {
     pub fn zero() -> Self {
         Integer { sign: Sign::PLUS, value: vec![] }
     }
 
     pub fn abs(self: &Self) -> Self {
-        let Integer { sign, value} = &self;
+        let Integer { value, .. } = &self;
 
         Integer {
             sign: Sign::PLUS,
@@ -111,15 +117,20 @@ impl Integer {
             let n1 = v1.get(i).map_or(0, |&n| n);
             let n2 = v2.get(i).map_or(0, |&n| n);
             
-            let space = u32::MAX - n1;
+            let (n, c) = match (n1, n2, carry) {
+                (_, u32::MAX, 1) => (n1, 1),
+                (u32::MAX, _, 1) => (n2, 1),
+                _ => {
+                    if n1 < u32::MAX - n2 - carry {
+                        (n1 + n2 + carry, 0)
+                    } else {
+                        (n1 - (u32::MAX - n2 - carry) - 1, 1)
+                    }
+                }
+            };
 
-            if n2 < space - carry {
-                value.push(n1 + n2 + carry);
-                carry = 0;
-            } else {
-                value.push(n1 - space + n2 + carry - 1);
-                carry = 1;
-            }
+            value.push(n);
+            carry = c;
         }
 
         if carry > 0 {
@@ -137,22 +148,35 @@ impl Integer {
         };
 
         let mut value: Vec<u32> = vec![];
-        let mut borrow: u32 = 0;
+        let mut borrow: u32 = 0;    // 0 or 1
 
         for i in 0..max(v1.len(), v2.len()) {
             let n1 = v1.get(i).map_or(0, |&n| n);
             let n2 = v2.get(i).map_or(0, |&n| n);
 
-            if n1 >= n2 && n1 - n2 > borrow {
-                value.push(n1 - n2);
-                borrow = 0;
-            } else {
-                value.push(u32::MAX - n2 + 1 - borrow + n1);
-                borrow = 1;
-            }
+            let [n1, n2] = [n1, n2].map(|n| n - min(n1, n2));
+
+            let (n, b) = match (n1, n2, borrow) {
+                (0, 0, 0) => (0, 0),
+                (0, 0, 1) => (u32::MAX, 1),
+                (0, _, 0) => (u32::MAX - n2 + 1, 1),
+                (0, _, 1) => (u32::MAX - n2, 1),
+                _ => (n1 - borrow, 0),
+            };
+
+            value.push(n);
+            borrow = b;
+        }
+
+        while let Some(0) = value.last() {
+            value.pop();
         }
 
         Integer { sign, value }
+    }
+
+    fn with_sign(self, sign: Sign) -> Self {
+        Integer { sign, value: self.value }
     }
 }
 
@@ -169,6 +193,62 @@ impl From<i32> for Integer {
                 value: vec![v.abs().try_into().unwrap()],
             }
         }
+    }
+}
+
+impl From<u32> for Integer {
+    fn from(v: u32) -> Self {
+        match v {
+            0 => Integer::zero(),
+            _ => Integer {
+                sign: Sign::PLUS,
+                value: vec![v],
+            }
+            
+        }
+    }
+}
+
+impl From<&str> for Integer {
+    fn from(s: &str) -> Self {
+        // TODO: Support for various number formats
+        // TODO: Validate string.
+        let (sign, s) = match s.chars().nth(0) {
+            Some('-') => (Sign::MINUS, &s[1..]),
+            Some('+') => (Sign::PLUS, &s[1..]),
+            _ => (Sign::PLUS, s),
+        };
+
+        let chunk_size: usize = MAX_POW_10_EXP.try_into().expect(
+            "16-bit architecture is not supported."
+        );
+
+        let mut chunks: Vec<&str> = vec![];
+
+        let mut i: usize = 0;
+        let mut j: usize = s.len() % chunk_size;
+
+        while j <= s.len() {
+            chunks.push(&s[i..j]);
+            (i, j) = (j, j + chunk_size);
+        }
+
+        let from_chunk = |s: &str, i: usize| -> Integer {
+            let uint = s
+                .parse::<u32>()
+                .expect("str validation not yet implemented");
+            
+            (uint * MAX_POW_10_VALUE.pow(i.try_into().unwrap())).into()
+        };
+
+        chunks
+            .into_iter()
+            .rev()
+            .enumerate()
+            .fold(Integer::zero(), |acc, (i, s)| {
+                acc + from_chunk(s, i)
+            })
+            .with_sign(sign)
     }
 }
 
@@ -318,6 +398,71 @@ mod test_integer_from_i32 {
 }
 
 #[cfg(test)]
+mod test_integer_from_str {
+    use super::*;    
+
+    #[test]
+    fn zero() {
+        let ans = Integer::zero();
+        assert_eq!(ans, "0".into());
+    }
+
+    #[test]
+    fn zero_minus() {
+        let ans = Integer::zero();
+        assert_eq!(ans, "-0".into());
+    }
+
+    #[test]
+    fn pos_abs_small_unsigned() {
+        let ans: Integer = 100.into();
+        assert_eq!(ans, "100".into());
+    }
+
+    #[test]
+    fn pos_abs_small_signed() {
+        let ans: Integer = 100.into();
+        assert_eq!(ans, "+100".into());
+    }
+
+    #[test]
+    fn neg_abs_small() {
+        let ans: Integer = (-100).into();
+        assert_eq!(ans, "-100".into());
+    }
+
+    #[test]
+    fn pos_abs_large_unsigned() {
+        let s = "4294967296";
+        let ans = Integer {
+            sign: Sign::PLUS,
+            value: vec![4294967295, 1],
+        };
+        assert_eq!(ans, s.into());
+    }
+
+    #[test]
+    fn pos_abs_large_signed() {
+        let s = "+4294967296";
+        let ans = Integer {
+            sign: Sign::PLUS,
+            value: vec![1, 1],
+        };
+        assert_eq!(ans, s.into());
+    }
+
+    #[test]
+    fn neg_abs_large() {
+        let s = "-4294967297";
+        let ans = Integer {
+            sign: Sign::PLUS,
+            value: vec![2, 1],
+        };
+        assert_eq!(ans, s.into());
+    }
+}
+
+#[cfg(test)]
 mod test_integer_ops {
     use super::*;
 
@@ -337,10 +482,21 @@ mod test_integer_ops {
 
     #[test]
     fn add_abs() {
-        let v1: Integer = (-9999).into();
-        let v2: Integer = 99.into();
-        let ans: Integer = 10_098.into();
+        let v1: Integer = (-9899).into();
+        let v2: Integer = 119.into();
+        let ans: Integer = 10_018.into();
         assert_eq!(ans, Integer::add_abs(&v1, &v2));
+    }
+
+    #[test]
+    fn add_abs_large() {
+        let v1: Integer = i32::MIN.into();
+        let v2: Integer = i32::MIN.into();
+
+        let ans: Vec<u32> = vec![0, 1];
+        let Integer { value: res, .. } = Integer::add_abs(&v1, &v2);
+
+        assert_eq!(ans, res);
     }
 
     #[test]
@@ -359,6 +515,16 @@ mod test_integer_ops {
         assert_eq!(ans, Integer::sub_abs(&v1, &v2));
     }
 
+    #[test]
+    fn sub_abs_pos_abs_large() {
+        let v1: Integer = Integer { sign: Sign::MINUS, value: vec![1, 1] };
+        let v2: Integer = Integer { sign: Sign::MINUS, value: vec![0, 1, 1] };
+        let ans = Integer { sign: Sign::MINUS, value: vec![u32::MAX, u32::MAX] };
+        
+        assert_eq!(format!("{}", ans), "-18446744073709551615");
+        assert_eq!(ans, Integer::sub_abs(&v1, &v2));
+    }
+    
     #[test]
     fn add_small_pos_pos() {
         let v1: Integer = 510.into();
@@ -411,6 +577,40 @@ mod test_integer_ops {
         let ans: Integer = (-8999).into();
 
         assert_eq!(ans, v1 + v2);
+    }
+
+    #[test]
+    fn add_large_pos_pos_fmt() {
+        let value = 1_u32 << 31;
+
+        assert_eq!(value, 2_147_483_648);
+
+        let v1: Integer = value.into();
+        let v2: Integer = value.into();
+        let ans = "4294967296";
+
+        assert_eq!(ans, &format!("{}", v1 + v2));
+    }
+
+    #[test]
+    fn add_large_neg_neg_fmt() {
+        let value = i32::MIN;
+
+        let v1: Integer = value.into();
+        let v2: Integer = value.into();
+        let ans = "-4294967296";
+
+        assert_eq!(ans, &format!("{}", v1 + v2));
+    }
+
+    #[test]
+    fn add_large_pos_neg_fmt() {
+        todo!();
+    }
+
+    #[test]
+    fn add_large_neg_pos_fmt() {
+        todo!();
     }
 }
 
