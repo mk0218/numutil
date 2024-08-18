0a098e47f7166a7c6c419bec68ca61461dc904d2
Minkyoung Kim <minkyoung.kim@alsemy.com>
2024-08-18 16:41:44 +0900

    Implement multiplication.

--- src/integer/ops.rs
+++ src/integer/ops.rs
@@ -1,5 +1,5 @@
 use std::{
-    cmp::{min, max},
+    cmp::{max, min},
     ops
 };
 
@@ -74,9 +74,59 @@ impl Integer {
         Integer { sign, value }
     }
 
-    // fn mul_abs(self: &Self, other: &Self) -> Self {
+    /// Performs u8 integer addition.
+    /// Returns (carry, result mod 2^8).
+    fn add_u8(v1: u8, v2: u8) -> (u8, u8) {
+        let half = 1 << 7;
+        let (h1, v1) = (v1 / half, v1 % half);
+        let (h2, v2) = (v2 / half, v2 % half);
+        let (h3, v) = ((v1 + v2) / half, (v1 + v2) % half);
 
-    // }
+        match h1 + h2 + h3 {
+            3.. => (1, v + half),
+            2 => (1, v),
+            1 => (0, v + half),
+            0 => (0, v),
+        }
+    }
+
+    /// Returns `v * ((2 ^ 8) ^ cnt)`.
+    fn shl_value(v: Integer, cnt: usize) -> Integer {
+        let Integer { sign, mut value } = v;
+
+        if cnt > 0 {
+            let mut result = vec![0; cnt];
+            result.append(&mut value);
+            value = result;
+        }
+
+        Integer { sign, value }
+    }
+    
+    /// Performs u8 integer multiplication.
+    /// Returns (carry, result mod 2^8).
+    fn mul_u8(v1: u8, v2: u8) -> (u8, u8) {
+        let div_16 = |v| {
+            (v >> 4, v - ((v >> 4) << 4))
+        };
+
+        dbg!(v1, v2);
+
+        let (v1_l, v1_r) = div_16(v1);
+        let (v2_l, v2_r) = div_16(v2);
+
+        let (t1, t4) = (v1_l * v2_l, v1_r * v2_r);
+        let (t2, t3) = (v1_l * v2_r, v1_r * v2_l);
+
+        let (t2_l, t2_r) = div_16(t2);
+        let (t3_l, t3_r) = div_16(t3);
+
+        let c1 = t1 + t2_l + t3_l;
+        let (c2, r1) = Integer::add_u8(t2_r << 4, t3_r << 4);
+        let (c3, r2) = Integer::add_u8(r1, t4);
+
+        (c1 + c2 + c3, r2)
+    }
 }
 
 impl ops::Neg for Integer {
@@ -118,7 +168,45 @@ impl ops::Mul for Integer {
     type Output = Integer;
 
     fn mul(self, rhs: Self) -> Self::Output {
-        0_u8.into()
+        let (v1, v2) = (self, rhs);
+        let (s1, s2) = (v1.sign, v2.sign);
+
+        if v1 == 0.into() || v2 == 0.into() { 0.into() }
+        else if v1.abs() == 1.into() { v2.with_sign(s1 * s2) }
+        else if v2.abs() == 1.into() { v1.with_sign(s1 * s2) }
+        else if v1.value.len() == 1 && v2.value.len() == 1 {
+            let v1: u8 = v1.abs().try_into().unwrap();
+            let v2: u8 = v2.abs().try_into().unwrap();
+
+            let value = match Integer::mul_u8(v1, v2) {
+                (0, 0) => vec![],
+                (0, r) => vec![r],
+                (c, r) => vec![r, c],
+            };
+
+            Integer { sign: s1 * s2, value }
+        } else {
+            let (v1, v2) = (v1.value, v2.value);
+
+            let v1_l = Integer { sign: s1, value: v1[..(v1.len() / 2)].to_vec() };
+            let v1_r = Integer { sign: s1, value: v1[(v1.len() / 2)..].to_vec() };
+            let v2_l = Integer { sign: s2, value: v2[..(v2.len() / 2)].to_vec() };
+            let v2_r = Integer { sign: s2, value: v2[(v2.len() / 2)..].to_vec() };
+
+            let (l1, l2) = (v1_l.value.len(), v2_l.value.len());
+
+            let t1 = v1_l.clone() * v2_l.clone();
+            let t2 = v1_l.clone() * v2_r.clone();
+            let t3 = v1_r.clone() * v2_l.clone();
+            let t4 = v1_r.clone() * v2_r.clone();
+
+            dbg!(&t1, &t2, &t3, &t4);
+
+            t1
+                + Integer::shl_value(t2, l2)
+                + Integer::shl_value(t3, l1)
+                + Integer::shl_value(t4, l1 + l2)
+        }
     }
 }
 
@@ -148,6 +236,54 @@ mod test_utils {
         let res = Integer::sub_abs(&v1.into(), &v2.into());
         let ans: Integer = (-40000).into();
         assert_eq!(ans, res);
+    }
+
+    #[test]
+    fn add_u8_small_1() {
+        let (v1, v2) = (127, 127);
+        let res = Integer::add_u8(v1, v2);
+        let ans = (0, 254);
+        assert_eq!(ans, res);
+    }
+    
+    #[test]
+    fn add_u8_large_1() {
+        let (v1, v2) = (128, 127);
+        let res = Integer::add_u8(v1, v2);
+        let ans = (0, 255);
+        assert_eq!(ans, res);
+    }
+
+    #[test]
+    fn add_u8_large_2() {
+        let (v1, v2) = (255, 255);
+        let res = Integer::add_u8(v1, v2);
+        let ans = (1, 254);
+        assert_eq!(ans, res);
+    }
+
+    #[test]
+    fn mul_u8() {
+        let (v1, v2) = (15, 15);
+        let res = Integer::mul_u8(v1, v2);
+        let ans = (0, 225);
+        assert_eq!(res, ans);
+    }
+
+    #[test]
+    fn mul_u8_large2() {
+        let (v1, v2) = (200, 15);
+        let res = Integer::mul_u8(v1, v2);
+        let ans = (11, 184);
+        assert_eq!(res, ans);
+    }
+
+    #[test]
+    fn mul_u8_large3() {
+        let (v1, v2) = (255, 255);
+        let res = Integer::mul_u8(v1, v2);
+        let ans = (254, 1);
+        assert_eq!(res, ans);
     }
 }
 
@@ -269,7 +405,42 @@ mod test_sub {
     }
 }
 
-// #[cfg(test)]
-// mod test_mul {
-//     fn 
-// }
+#[cfg(test)]
+mod test_mul {
+    use super::*;
+    
+    #[test]
+    fn mul() {
+        let (v1, v2) = (Integer::from(100), Integer::from(100));
+        let ans = Integer::from(10000);
+        assert_eq!(v1 * v2, ans);
+    }
+
+    #[test]
+    fn mul_large() {
+        let (v1, v2) = (Integer::from(10), Integer::from(10_000));
+        let ans = Integer::from(100_000);
+        assert_eq!(v1 * v2, ans);
+    }
+    
+    #[test]
+    fn mul_larger() {
+        let (v1, v2) = (Integer::from(10_000), Integer::from(10_000));
+        let ans = Integer::from(100_000_000);
+        assert_eq!(v1 * v2, ans);
+    }
+
+    #[test]
+    fn mul_pos_neg() {
+        let (v1, v2) = (Integer::from(-10), Integer::from(10_000));
+        let ans = Integer::from(-100_000);
+        assert_eq!(v1 * v2, ans);
+    }
+
+    #[test]
+    fn mul_neg_neg() {
+        let (v1, v2) = (Integer::from(-10_000), Integer::from(-10_000));
+        let ans = Integer::from(100_000_000);
+        assert_eq!(v1 * v2, ans);
+    }
+}
